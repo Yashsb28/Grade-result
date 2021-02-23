2d942a399e6d6cd300dba20169d05d51baecf1f0
Yashsb28 <78728820+Yashsb28@users.noreply.github.com>
2021-02-23 20:52:11 +0530

    Create grade.cpp

---
+++ grade.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("Enter your marks : \n");
+  scanf("%d", &n);
+  if(n>=85)
+  {
+  	printf("Grade A");
+  }
+  else if(n>=70)
+  {
+  	printf("Grade B");
+  }
+  else if(n>=55)
+  {
+  	printf("Grade C");
+  }
+  else if(n>=40)
+  {
+  	printf("Grade D");
+  }
+  else 
+  {
+  	printf("Grade F");
+  }
+  return 0;
+}
